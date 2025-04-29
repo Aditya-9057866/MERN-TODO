8f062092dfc9128bda1516f8a9f426ffb7fcec4e
Aditya <adityapratapsingh7@gmail.com>
2025-04-30 04:20:38 +0530

    frontend

---
+++ Frontend/src/Hero.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <>
+        <div className="landing-container">
+      <div className="overlay"></div>
+      <div className="content">
+        <h1>Welcome to Our Platform</h1>
+        <div className="buttons">
+       
+            
+            <button className="btn btn-light">Login</button>
+
+          
+            <button className="btn btn-light">Sign Up</button>
+         
+        </div>
+      </div>
+    </div>
+
+</>
+    
+  )
+   
+  
+}
+
+export default Hero
